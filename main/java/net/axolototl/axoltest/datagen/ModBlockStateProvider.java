1754a2e5e9149342b863fe23c14b03eb525401b4
Axol <jdavidvargasc26@gmail.com>
2024-08-19 16:03:40 -0500

    Add files via upload

---
+++ main/java/net/axolototl/axoltest/datagen/ModBlockStateProvider.java
@@ -0,0 +1,44 @@
+package net.axolototl.axoltest.datagen;
+
+import net.axolototl.axoltest.AxolTest;
+import net.axolototl.axoltest.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.level.block.StairBlock;
+import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
+import net.neoforged.neoforge.client.model.generators.ModelFile;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.registries.DeferredBlock;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, AxolTest.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.BLACK_OPAL_BLOCK);
+        blockWithItem(ModBlocks.RAW_BLACK_OPAL_BLOCK);
+        blockWithItem(ModBlocks.BLACK_OPAL_ORE);
+        blockWithItem(ModBlocks.BLACK_OPAL_DEEPSLATE_ORE);
+        blockWithItem(ModBlocks.BLACK_OPAL_NETHER_ORE);
+        blockWithItem(ModBlocks.BLACK_OPAL_END_ORE);
+
+        blockWithItem(ModBlocks.MAGIC_BLOCK);
+
+        stairsBlock(((StairBlock) ModBlocks.BLACK_OPAL_STAIRS.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
+        slabBlock(((SlabBlock) ModBlocks.BLACK_OPAL_SLAB.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()), blockTexture(ModBlocks.BLACK_OPAL_BLOCK.get()));
+
+        blockItem(ModBlocks.BLACK_OPAL_SLAB);
+        blockItem(ModBlocks.BLACK_OPAL_STAIRS);
+    }
+
+    private void blockWithItem(DeferredBlock<Block> deferredBlock){
+        simpleBlockWithItem(deferredBlock.get(), cubeAll(deferredBlock.get()));
+    }
+
+    private void blockItem(DeferredBlock<Block> deferredBlock) {
+        simpleBlockItem(deferredBlock.get(), new ModelFile.UncheckedModelFile("axoltest:block/" + deferredBlock.getId().getPath()));
+    }
+}
---
+++ main/java/net/axolototl/axoltest/datagen/ModItemModelProvider.java
@@ -0,0 +1,23 @@
+package net.axolototl.axoltest.datagen;
+
+import net.axolototl.axoltest.AxolTest;
+import net.axolototl.axoltest.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, AxolTest.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        basicItem(ModItems.BLACK_OPAL.get());
+        basicItem(ModItems.RAW_BLACK_OPAL.get());
+        basicItem(ModItems.CHAINSAW.get());
+        basicItem(ModItems.TOMATO.get());
+        basicItem(ModItems.FROSTFIRE_ICE.get());
+    }
+
+}
