541efc222557828a85af4d3ef06bb240fe12d39b
Axol <jdavidvargasc26@gmail.com>
2024-08-19 20:59:16 -0500

    Add files via upload

---
+++ java/net/axolototl/axoltest/item/ModFoodProperties.java
@@ -0,0 +1,13 @@
+package net.axolototl.axoltest.item;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoodProperties {
+    public static final FoodProperties TOMATO = new FoodProperties.Builder().nutrition(3).saturationModifier(2)
+            .effect(() -> new MobEffectInstance(MobEffects.LUCK, 2400), 1f).build();
+}
+// Para añadir bebidas se debe de hacer una custom clase
+
